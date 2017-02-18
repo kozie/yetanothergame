f9da9c220ccc9f521253a28268bca1e442cb93e0
Koos van Egmond <koos@cream.nl>
2017-02-18 13:58:06 +0100

    Added rendering of scaled images withouth anti aliasing

--- src/main.js
+++ src/main.js
@@ -1,21 +1,30 @@
 import * as PIXI from 'pixi.js';
 
 const root = document.getElementById('root');
-const renderer = PIXI.autoDetectRenderer(640, 480);
+const renderer = PIXI.autoDetectRenderer(640, 480, {antialias: false});
+PIXI.SCALE_MODES.DEFAULT = PIXI.SCALE_MODES.NEAREST;
 
 root.appendChild(renderer.view);
 
 const stage = new PIXI.Container();
+stage.scale.x = stage.scale.y = 3;
 
 const assets = require('../assets/assets.js');
 for (let key in assets) {
     if (assets.hasOwnProperty(key)) {
-        PIXI.loader.add(assets[key]);
+        PIXI.loader.add(key, assets[key]);
     }
 }
 
 PIXI.loader.once('complete', () => {
     console.log('done loading');
+
+    const test = new PIXI.Sprite(PIXI.loader.resources.test.texture);
+    test.x = 64;
+    test.y = 32;
+
+    stage.addChild(test);
+    
     renderer.render(stage);
 });
 
