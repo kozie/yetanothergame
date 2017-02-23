50ba1abc64adfb849750a428acbdd9109cbbb92f
Koos van Egmond <koos@cream.nl>
2017-02-23 11:13:57 +0100

    Added dist which enabled subtree pushing to gh-pages

--- src/Scene/StartScene.js
+++
@@ -1,9 +0,0 @@
-import Scene from '../Scene';
-import TestEntity from '../Entity/TestEntity';
-
-export default class StartScene extends Scene {
-    init() {
-        // Add Test entity
-        this.addEntity(new TestEntity(3, 3));
-    }
-}
